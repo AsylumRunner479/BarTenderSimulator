080b599d1a770c2da9584990f7865123edf7e506
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-10-21 15:23:20 +1100

    added vr rig

---
+++ Assets/Scripts/VrRig.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VrRig : MonoBehaviour
+{
+    public static VrRig instance = null;
+
+    public Transform Headset { get { return headset; } }
+    public Transform LeftController { get { return leftController; } }
+    public Transform RightController { get { return rightController; } }
+
+    [SerializeField]
+    private Transform headset;
+    [SerializeField]
+    private Transform leftController;
+    [SerializeField]
+    private Transform rightController;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+
+}
