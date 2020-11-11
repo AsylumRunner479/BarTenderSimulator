025bd4099bdd512b08edab6c209da93340394d78
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-11-11 16:03:27 +1100

    pushing the mug

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -7,9 +7,17 @@ public class Teleporter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        Pointer[] pointers = FindObjectsOfType<Pointer>();
+        foreach (Pointer pointer in pointers)
+        {
+            pointer.BeamMeUpScotty.AddListener(BeamMeUpScotty);
+        }
 
+    }
+    private void BeamMeUpScotty(Vector3 _position)
+    {
+        transform.position = _position;
+    }
     // Update is called once per frame
     void Update()
     {
