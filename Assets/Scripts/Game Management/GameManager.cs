ddcad89f858c816ca8d5aca04b264c3bdac16715
tek-f <aaron.falco2@gmail.com>
2020-11-18 15:20:14 +1100

    Update

--- Assets/Scripts/Game Management/GameManager.cs
+++ Assets/Scripts/Game Management/GameManager.cs
@@ -6,7 +6,24 @@ namespace Underdrunk.GameManagement
 {
     public class GameManager : MonoBehaviour
     {
+        #region Orders
+        [SerializeField] Transform orderDisplayPanel;
         [SerializeField] GameObject orderButtonPrefab;
         public List<OrderBehaviours> currentOrders = new List<OrderBehaviours>();
+        #endregion
+
+        void AddOrder()
+        {
+            OrderBehaviours newOrder = Instantiate(orderButtonPrefab, orderDisplayPanel).GetComponent<OrderBehaviours>();
+            currentOrders.Add(newOrder);
+        }
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.A))
+            {
+                AddOrder();
+            }
+        }
     }
 }
