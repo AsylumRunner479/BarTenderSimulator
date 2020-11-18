3198e5e997f539781f8c84534047e436bff1e133
tek-f <aaron.falco2@gmail.com>
2020-11-18 14:29:58 +1100

    Aaron Update Created Scripts
    Created GameManagement and OrderBehaviours scripts, created a prototype display screen in TestScene_GameManager

---
+++ Assets/Scripts/Game Management/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Underdrunk.GameManagement
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] GameObject orderButtonPrefab;
+        public List<OrderBehaviours> currentOrders = new List<OrderBehaviours>();
+    }
+}
---
+++ Assets/Scripts/Game Management/OrderBehaviours.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Underdrunk.GameManagement
+{
+    public class OrderBehaviours : MonoBehaviour
+    {
+        public Text orderText;
+        public Text timeText;
+        public List<string> possibleOrders = new List<string>();
+        public string order;
+        float orderTimeStamp;
+
+
+        private void Start()
+        {
+            orderTimeStamp = Time.time;
+            order = possibleOrders[Random.Range(0, possibleOrders.Count - 1)];
+            orderText.text = "Order: " + order;
+            if (timeText)
+            {
+                timeText.text = "Time: " + 0;
+            }
+        }
+
+        private void Update()
+        {
+            if (timeText)
+            {
+                int timeSinceOrder = (int)(Time.time - orderTimeStamp);
+                timeText.text = "Time: " + timeSinceOrder.ToString();
+            }
+        }
+    }
+}
