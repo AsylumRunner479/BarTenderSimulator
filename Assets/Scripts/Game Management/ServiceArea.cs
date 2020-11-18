a6af170ef1437999c9c2a02ced180888e9e666f2
tek-f <aaron.falco2@gmail.com>
2020-11-18 16:09:26 +1100

    Update Aaron

---
+++ Assets/Scripts/Game Management/ServiceArea.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Underdrunk.GameManagement
+{
+    public class ServiceArea : MonoBehaviour
+    {
+        GameManager gameManager;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            OrderBehaviours servedOrder = collision.gameObject.GetComponent<OrderBehaviours>();
+            if (servedOrder)
+            {
+                gameManager.CheckOrder(servedOrder);
+            }
+        }
+    }
+}
