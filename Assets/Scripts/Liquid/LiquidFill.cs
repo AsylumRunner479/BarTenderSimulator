7637681bf1bf032db60c71ba2e78f5aee160a836
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-11-25 13:59:32 +1100

    added code comments

--- Assets/Scripts/Liquid/LiquidFill.cs
+++ Assets/Scripts/Liquid/LiquidFill.cs
@@ -6,23 +6,29 @@ namespace Underdrunk.GameManagement
 {
     public class LiquidFill : MonoBehaviour
     {
-
+        //what is the top part of the mug, bottom part and where the liquid is relative to them
         public GameObject MugTop, MugBottom, self;
+        //how much liquid is in the cup and how much liquid can be in the cup
         public float fillAmount, maxfill = 1000;
+        //whether the cup is full or not
         public bool full;
+        //what type of liquid is in the liquid
         public string LiquidType;
         // Start is called before the first frame update
         void Start()
         {
+            //sets the liquid amount to 1
             fillAmount = 1;
         }
 
         public void FillLiquid()
         {
+            //if the fill amount is high enough then register it as full
             if (fillAmount >= maxfill)
             {
                 full = true;
             }
+            //increases the fill amount slowly
             if (fillAmount <= maxfill)
             {
                 fillAmount += 1;
@@ -33,7 +39,7 @@ namespace Underdrunk.GameManagement
         void Update()
         {
 
-
+            //makes the liquid move closer to the top/ further from the bottom
             self.transform.position = Vector3.Lerp(MugBottom.transform.position, MugTop.transform.position, Mathf.Clamp01(fillAmount / maxfill));
 
         }
--- Assets/Scripts/Liquid/PourAmount.cs
+++ Assets/Scripts/Liquid/PourAmount.cs
@@ -6,15 +6,23 @@ namespace Underdrunk.GameManagement
 {
     public class PourAmount : MonoBehaviour
     {
+        //the particle system that emits particles
         public ParticleSystem particle;
+        //how fast it is pouring and how fast is the max pour
         public float PourSpeed, maxPourSpeed, currentPour;
+        //whether the tap is on or not
         public bool TapOn;
+        //what is the liquid being affected
         public LiquidFill liquid;
+        //what is the mesh that can be affected
         public MeshRenderer Mesh;
+        //what is the material(color) of the liquid
         public Material material;
+        //what is the name of the liquid coming out of particle system
         public string LiquidType;
         public void ChangePour(float tap)
         {
+            //increase the tap speed up to a limit and decrease it till it hits 0
             if (tap != PourSpeed)
             {
                 if (tap <= maxPourSpeed)
@@ -33,6 +41,7 @@ namespace Underdrunk.GameManagement
         }
         private void OnParticleCollision(GameObject other)
         {
+            //increases the fill amount of a liquid whilst altering the material to fit this material
             liquid = other.GetComponent<LiquidFill>();
             if (liquid != null)
             {
@@ -40,6 +49,7 @@ namespace Underdrunk.GameManagement
             }
             if (liquid.fillAmount >= liquid.maxfill)
             {
+                //when the cup is full bounces materials everywhere
                 var bouncy = particle.collision;
                 bouncy.bounce = 1;
                 bouncy.dampen = 0;
@@ -51,6 +61,7 @@ namespace Underdrunk.GameManagement
         }
         private void Update()
         {
+            //when tap is on increase current pour
             if (TapOn)
             {
                 currentPour += 10 * Time.deltaTime;
@@ -66,11 +77,13 @@ namespace Underdrunk.GameManagement
                 currentPour = maxPourSpeed - 1;
                 ChangePour(currentPour);
             }
+            //decreases the current pour when the tap is off
             else
             {
                 currentPour -= 20 * Time.deltaTime;
                 ChangePour(currentPour);
             }
+            //sets the particle emission based on the pour speed
             var rate = particle.emission;
             rate.rateOverTime = PourSpeed;
         }
