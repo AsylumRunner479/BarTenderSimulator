68e3f6ac154169b0c56e74cbc85b386e827a09a3
tek-f <60990350+tek-f@users.noreply.github.com>
2020-12-02 14:39:05 +1100

    Update

--- Assets/Scripts/Game Management/MugBehavior.cs
+++ Assets/Scripts/Game Management/MugBehavior.cs
@@ -7,35 +7,17 @@ namespace Underdrunk.GameManagement
     public class MugBehavior : MonoBehaviour
     {
         LiquidFill liquidFill;
-        float gravity = 2;
-        bool falling = true;
         private void OnTriggerEnter(Collider other)
         {
             if(other.transform.CompareTag("ServiceArea"))
             {
                 GameManager.singleton.CheckOrder(liquidFill.LiquidType);
+                Destroy(gameObject);
             }
-        }
-        private void OnCollisionEnter(Collision collision)
-        {
-            falling = false;
-        }
-        private void OnCollisionExit(Collision collision)
-        {
-            falling = true;
         }
         private void Start()
         {
             liquidFill = GetComponent<LiquidFill>();
-        }
-        private void Update()
-        {
-            if (falling)
-            {
-                Vector3 pos = transform.position;
-                pos.y -= (gravity * Time.deltaTime);
-                transform.position = pos;
-            }
         }
     }
 }
